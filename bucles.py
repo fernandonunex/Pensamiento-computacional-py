25c1c9f5480d955f31d4b85ae5696c77f1b631a1
FernandoOmen <fer_bsof@outlook.com>
2020-08-12 01:08:38 -0500

    Using the function iter() and next()

--- bucles.py
+++ bucles.py
@@ -3,9 +3,19 @@
 for day in week:
     print(f"The day is {day}") """
 
-iter('cadena') # cadena
+""" iter('cadena') # cadena
 iter(['a', 'b', 'c']) # lista
 iter(('a', 'b', 'c')) # tupla
 iter({'a', 'b', 'c'}) # conjunto
-iter({'a': 1, 'b': 2, 'c': 3}) # diccionario
+iter({'a': 1, 'b': 2, 'c': 3}) # diccionario """
 
+week = ['M','T','W','T','F','S','S']
+iterator = iter(week)
+next(iterator)
+next(iterator)
+next(iterator)
+next(iterator)
+next(iterator)
+next(iterator)
+next(iterator)
+next(iterator)
