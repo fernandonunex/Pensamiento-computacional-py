8711b70b998fabbe7d55836f5ac9f39667996c65
FernandoOmen <fer_bsof@outlook.com>
2020-08-20 19:07:53 -0500

    Set the recursion limit and how to see it.

--- factorial.py
+++ factorial.py
@@ -1,3 +1,7 @@
+import sys
+sys.setrecursionlimit(1000)
+print(sys.getrecursionlimit())
+
 def factorial(n):
     """This function compute the factorial of n
 
@@ -10,5 +14,5 @@ def factorial(n):
     print(f'The value of n: {n} \n')
     return n*factorial(n-1)
 
-n = int(input('Enter a number to find its factorial: '))
-print(f'The factorial of is: {factorial(n)}')
+# n = int(input('Enter a number to find its factorial: '))
+# print(f'The factorial of is: {factorial(n)}')
